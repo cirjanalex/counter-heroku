ae23e419722f49de7e284fccb300e9f6fc02d20b
Alexandru Cirjan <cirjan@mds.microsoft.com>
2021-08-28 17:14:32 +0300

    improve the UI

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-
+import './Home.css';
 export class Home extends Component {
   static displayName = Home.name;
 
   constructor(props) {
     super(props);
-    this.state = { counter: [], loading: true };
+    this.state = { counter: [], loading: true, loadingIn: 10 };
     this.incrementCounter = this.incrementCounter.bind(this);
+    var i = 10;
     setInterval(() => {
-      this.fetchCounter();
-    }, 10000);
+      this.state.loadingIn = i;      
+      i--;
+      this.setState({ loadingIn: i });
+      if (i == 0) {
+        this.fetchCounter();
+        i = 10;
+      }
+    }, 1000);
   }
 
   componentDidMount() {
@@ -19,7 +26,7 @@ export class Home extends Component {
   async incrementCounter() {
     await fetch('api/counter', {
       method: 'POST'
-    });      
+    });
     await this.fetchCounter();
   }
 
@@ -30,17 +37,11 @@ export class Home extends Component {
   }
 
   render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : (<div>
-          <h2>{this.state.counter.value}</h2>
-          <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-        </div>)
-
     return (
       <div>
-        <h1>S1 Current Counter</h1>
-        {contents}
+        <h1>Current Counter: <span class="counterValue">{this.state.counter.value}</span></h1>        
+        <div>Refreshing in {this.state.loadingIn}</div>
+        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment from here</button>
       </div>
     );
   }
